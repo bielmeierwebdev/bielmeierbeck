98a47f14643287f576641cc15e264f4e2b7b24c1
bielmeierwebdev <bielmeierwebdevelop@gmail.com>
2026-05-13 22:07:59 +0200

    backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,28 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const port = process.env.PORT || 3000;
-
-  await app.listen(port);
-
-  console.log(`APP RUNNING ON ${port}`);
-
-  /*app.enableCors({
-    origin: ['https://bielmeierbeck-8k7g.vercel.app'],
-
-    credentials: true,
-  });*/
 
   app.enableCors({
     origin: true,
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  console.log(`APP RUNNING ON ${port}`);
 }
 
 bootstrap();
