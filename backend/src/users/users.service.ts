dc54c54fc547304b7756a4dce94be9977d62d153
bielmeierwebdev <bielmeierwebdevelop@gmail.com>
2026-05-15 14:44:17 +0200

    fix

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@nestjs/common';
+
 import { PrismaService } from 'prisma/prisma.service';
+
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  findByEmail(email: string) {
+  findByUsername(username: string) {
     return this.prisma.user.findUnique({
       where: {
-        email,
+        username,
       },
     });
   }
 
-  create(data: {
-    email: string;
-    password: string;
-    role?: 'ADMIN' | 'EMPLOYEE';
-  }) {
+  create(data: { username: string; password: string; role?: Role }) {
     return this.prisma.user.create({
       data,
     });
